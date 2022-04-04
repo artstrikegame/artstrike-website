c7d4297fefe9e0eed78db1c895e501b948531d1f
William <williamzhang8@live.ca>
2022-04-03 23:06:13 -0700

    final prerelease push

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { SocialIcon } from 'react-social-icons';
 
 export default function Home() {
